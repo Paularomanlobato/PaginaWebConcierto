c009b893a504b5fa762f125b36ef329bdc7a7dba
Paularomanlobato <paula.m.roman.lobato@avanade.com>
2022-12-01 00:06:06 +0100

    Agregar archivos de proyecto.

---
+++ src/app/contacto/contacto.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contacto-component',
+  templateUrl: './contacto.component.html',
+})
+export class ContactoComponent implements OnInit {
+  constructor() { }
+  ngOnInit(): void { }
+}
